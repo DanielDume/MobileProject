b939e52f478ab937239893a05544f7a80dc06d39
unknown <dume.daniel@ymail.com>
2017-10-03 13:43:00 +0300

    test

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class MobileProject extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native added stuff!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
